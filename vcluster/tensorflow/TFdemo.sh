5adc292af2764d2a1c0ede1b94b78938a7bf35ff
Li-YuanDi <lisoftdi@gmail.com>
2017-09-10 18:17:30 +0800

    fix bug

--- vcluster/tensorflow/TFdemo.sh
+++ vcluster/tensorflow/TFdemo.sh
@@ -9,7 +9,7 @@ if [[ "$HOSTNAME" == "TFnode0" ]]; then
 fi
 if [[ "$HOSTNAME" == "TFnode1" ]]; then
 	#statements
-	python /root/TensorflowBenchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --local_parameter_device=cpu --device=cpu --model=resnet50 --variable_update=distributed_replicated --job_name=worker --ps_hosts=172.17.0.2:50000 --worker_hosts=172.17.0.3:50001,172.17.0.4:50001,172.17.0.5:50001 --task_index=0  --data_format=NHWC --force_gpu_compatible=false --num_batches-10 --display_every=1
+	python /root/TensorflowBenchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --local_parameter_device=cpu --device=cpu --model=resnet50 --variable_update=distributed_replicated --job_name=worker --ps_hosts=172.17.0.2:50000 --worker_hosts=172.17.0.3:50001,172.17.0.4:50001,172.17.0.5:50001 --task_index=0  --data_format=NHWC --force_gpu_compatible=false --num_batches=10 --display_every=1
 fi
 if [[ "$HOSTNAME" == "TFnode2" ]]; then
 	#statements
