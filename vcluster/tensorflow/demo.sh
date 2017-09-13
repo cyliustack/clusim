5bcc8b2f452ec2b913db1e4d6be165340891826c
Li-YuanDi <lisoftdi@gmail.com>
2017-09-13 17:20:18 +0800

    generating ssh key to the number of nodes you pass to demo.sh

--- vcluster/tensorflow/demo.sh
+++ vcluster/tensorflow/demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-~/benchmark/tools/ssh_copy_id_to_all.sh 4
-~/benchmark/tools/genhosts.sh 4
+~/benchmark/tools/ssh_copy_id_to_all.sh $1
+~/benchmark/tools/genhosts.sh $1
 #mpirun -f hosts.txt benchmark/mpi/gemm/gemm 1000 1000 1000
 parallel-ssh -h hosts.txt -t 18000 "cd / && ./TFdemo.sh"
