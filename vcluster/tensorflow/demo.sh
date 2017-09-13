f0bbe56d0e5defb0c60ddceaaa55bba0523f41bc
Li-YuanDi <lisoftdi@gmail.com>
2017-09-13 17:21:48 +0800

    for 8 nodes use

--- vcluster/tensorflow/demo.sh
+++ vcluster/tensorflow/demo.sh
@@ -2,4 +2,4 @@
 ~/benchmark/tools/ssh_copy_id_to_all.sh $1
 ~/benchmark/tools/genhosts.sh $1
 #mpirun -f hosts.txt benchmark/mpi/gemm/gemm 1000 1000 1000
-parallel-ssh -h hosts.txt -t 18000 "cd / && ./TFdemo.sh"
+parallel-ssh -h hosts.txt -t 18000 "cd / && ./TF8nodes.sh"
