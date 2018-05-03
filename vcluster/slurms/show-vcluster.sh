8df8e00b532dac3a5eafebb1c17e9c36f1e33bcb
cyliustack <cyliustack@gmail.com>
2018-05-03 17:33:58 +0800

    update for slurms

---
+++ vcluster/slurms/show-vcluster.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+C_NONE="\033[0m"
+C_CYAN="\033[36m"
+C_RED="\033[31m"
+C_GREEN="\033[32m"
+C_ORANGE="\033[33m"
+C_BLUE="\033[34m"
+C_PURPLE="\033[35m"
+C_CYAN="\033[36m"
+C_LIGHT_GRAY="\033[37m"
+
+print_misc() {
+    echo -e "${C_PURPLE} $1 ${C_NONE}"
+}
+
+print_info() {
+    echo -e "${C_BLUE} $1 ${C_NONE}"
+}
+
+print_error() {
+    echo -e "${C_RED} $1 ${C_NONE}"
+}
+
+print_warning() {
+    echo -e "${C_ORANGE} $1 ${C_NONE}"
+}
+
+#print_misc "Bash version ${BASH_VERSION}."
+
+print_info "===== Docker-Image Information ====="
+sudo docker images -a 
+print_info "===== Docker-Container Information ====="
+sudo docker ps -a
