9b4f8e1b9b300923cd85fb7932dfd369c48c5534
James Liu <cyliustack@gmail.com>
2022-05-13 09:21:19 +0800

    add vim run

--- clusim-vm-run.go
+++ clusim-vm-run.go
@@ -4,6 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
+	"log"
+	"bytes"
+	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 var (
@@ -25,8 +30,38 @@ func usage() {
 
 func main() {
 	flag.Parse()
-	//arg0, _ := strconv.Atoi(flag.Arg(0))
+
 	if verbose {
 		fmt.Printf("image name : %s\n", imageName)
 	}
+
+	r := gin.Default()
+
+	gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandlers int) {
+			log.Printf("endpoint %v %v %v %v\n", httpMethod, absolutePath, handlerName, nuHandlers)
+	}
+
+	r.POST("/foo", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "foo")
+	})
+
+	r.GET("/bar", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "bar")
+	})
+
+	r.GET("/status", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "status")
+	})
+
+	os.Setenv("Path", os.Getenv("Path")+";C:\\Program Files\\qemu")
+	fmt.Println("Path = " + os.Getenv("Path"))
+
+	cmd := exec.Command("powershell", "qemu-system-x86_64 -smp 2 -m 2G -drive file=images/ubuntu-20.04.3.qcow2 -net user,hostfwd=tcp::10022-:22 -net nic -serial stdio")
+	var out bytes.Buffer
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("in all caps: %q\n", out.String())
+	//r.Run()
 }
